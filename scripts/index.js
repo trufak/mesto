fe3a718b696b2d3d4b7978a7c400c03b1c127f27
Sergey Trufakin <trufakin.sergey@mail.ru>
2022-05-11 22:39:07 +0700

    fix: добавлены метки aria-label

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+/*Переменные*/
+
 let profile = document.querySelector('.profile');
 let nameProfile = profile.querySelector('.profile__title');
 let descriptProfile = profile.querySelector('.profile__subtitle');
@@ -7,28 +9,33 @@ let popupForm = popup.querySelector('.popup__container');
 let popupNameProfile = popup.querySelector('.popup__input_title');
 let popupDescriptProfile = popup.querySelector('.popup__input_subtitle');
 let closePopupButton = popup.querySelector('.popup__close-button');
-/*Обработчик события click на кнопке editButton*/
-editButton.addEventListener ('click', popupOpen);
-/*Обработчик события lick на кнопке closePopupButton*/
-closePopupButton.addEventListener('click', popupClose);
-/*Обработчик события отправки данных формы popupForm*/
-popupForm.addEventListener('submit', popupFormSubmit);
-/*открыть popup*/
+
+/*Функции*/
+/*Открыть popup*/
 function popupOpen() {
   popupNameProfile.value = nameProfile.textContent;
   popupDescriptProfile.value = descriptProfile.textContent;
   if (!popup.classList.contains('popup_opened'))
     popup.classList.add('popup_opened');
 }
-/*закрыть popup*/
+/*Закрыть popup*/
 function popupClose () {
   if (popup.classList.contains('popup_opened'))
     popup.classList.remove('popup_opened');
 }
-/*отправка формы*/
+/*Отправка формы*/
 function popupFormSubmit (e) {
   e.preventDefault();
   nameProfile.textContent = popupNameProfile.value;
   descriptProfile.textContent = popupDescriptProfile.value;
   popupClose();
 }
+
+/*Обработчики событий
+/*Обработчик события click на кнопке editButton*/
+editButton.addEventListener ('click', popupOpen);
+/*Обработчик события lick на кнопке closePopupButton*/
+closePopupButton.addEventListener('click', popupClose);
+/*Обработчик события отправки данных формы popupForm*/
+popupForm.addEventListener('submit', popupFormSubmit);
+
