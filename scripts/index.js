956ec5f93ed79226b668d0a5522ee0de64970872
Sergey Trufakin <trufakin.sergey@mail.ru>
2022-05-18 10:50:43 +0700

    feat: добавить загрузку карточек при старте страницы

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,55 @@
 /*Переменные*/
 
-let profile = document.querySelector('.profile');
-let nameProfile = profile.querySelector('.profile__title');
-let descriptProfile = profile.querySelector('.profile__subtitle');
-let editButton = profile.querySelector('.profile__info-edit-button');
-let popup = document.querySelector('.popup');
-let popupForm = popup.querySelector('form[name=popup]');
-let popupNameProfile = popup.querySelector('.popup__input_type_name');
-let popupDescriptProfile = popup.querySelector('.popup__input_type_desc');
-let closePopupButton = popup.querySelector('.popup__close-button');
+const profile = document.querySelector('.profile');
+const nameProfile = profile.querySelector('.profile__title');
+const descriptProfile = profile.querySelector('.profile__subtitle');
+const editButton = profile.querySelector('.profile__info-edit-button');
+const popup = document.querySelector('.popup');
+const popupForm = popup.querySelector('form[name=popup]');
+const popupNameProfile = popup.querySelector('.popup__input_type_name');
+const popupDescriptProfile = popup.querySelector('.popup__input_type_desc');
+const closePopupButton = popup.querySelector('.popup__close-button');
+const elements = document.querySelector('.elements');
+const cardTemplate = document.querySelector('#card').content;
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 /*Функции*/
+/*Загрузка карточек при старте страницы*/
+function initialCardsLoad() {
+  let card;
+  initialCards.forEach (initData => {
+    card = cardTemplate.querySelector('.element-shild').cloneNode(true);
+    card.querySelector('.element__mask').src = initData.link;
+    card.querySelector('.element__mask').alt = initData.name;
+    card.querySelector('.element__caption').textContent = initData.name;
+    elements.append(card);
+  });
+}
 /*Открыть popup*/
 function popupOpen() {
   popupNameProfile.value = nameProfile.textContent;
@@ -39,3 +78,5 @@ closePopupButton.addEventListener('click', popupClose);
 /*Обработчик события отправки данных формы popupForm*/
 popupForm.addEventListener('submit', popupFormSubmit);
 
+/*Действия при загрузке страницы*/
+initialCardsLoad();
